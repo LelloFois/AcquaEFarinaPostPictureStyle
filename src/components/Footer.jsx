92697783fc45e067fdff117734c7976f349d5337
Antonello Fois <antonello.fois1381@gmail.com>
2022-10-13 12:12:42 +0200

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,33 @@
+
+
+export default function Footer() {
+    return(
+        <div className="footer">
+            <div className="footer__text">
+                <div className="footer__text_1">
+                    <h2>NÁJDETE NÁS:</h2>
+                    <p>
+                        Slnečnicová 1, Rovinka
+                    </p>
+                </div>
+                <div className="footer__text_2">
+                    <h2>OTVÁRACIE HODINY:</h2>
+                    <p>Po-Št:	11:00 - 22:00</p>
+                    <p>Pia-So:	11:00 - 23:00</p>
+                    <p>Ne:	11:00 - 22:00</p>
+                    
+                </div>
+                <div className="footer__text_3">
+                    <h2>KONTAKTUJTE NÁS:</h2>
+                    <p>Mail:	info@afpizza.sk</p>
+                    <p>Tel.:	0911 390 123</p>
+                </div>
+            </div>
+            <div class="bottom-wave">
+                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                    <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+                </svg>
+            </div>
+        </div>
+    )
+}
